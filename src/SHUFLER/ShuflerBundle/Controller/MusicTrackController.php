e000c289fe526a60eb2846e3775f26bd88f4bcb9
redfox <fred_ric@hotmail.com>
2018-09-03 22:19:20 +0200

    -

--- src/SHUFLER/ShuflerBundle/Controller/MusicTrackController.php
+++ src/SHUFLER/ShuflerBundle/Controller/MusicTrackController.php
@@ -5,7 +5,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use SHUFLER\ShuflerBundle\Entity\MusicTrack;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class MusicTrackController extends Controller
